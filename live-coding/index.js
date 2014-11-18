c2196766635498d07407f1cf127ff33d090ed50b
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2014-11-18 22:03:51 +0100

    add live coding

---
+++ live-coding/index.js
@@ -0,0 +1,19 @@
+// Build your own angular !
+
+// Scope
+//  => $$watchers
+//  => $watch
+//  => $digest
+//  => $apply
+
+
+// Compiler & Directives
+//  => $compile
+//  => $$directives
+
+
+// Tests & logs
+
+var $rootScope = new Scope();
+$rootScope.name = 'hello';
+
