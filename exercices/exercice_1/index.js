270d8a987d720afa55f159e61632ffc537229a4a
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2015-10-11 20:33:02 +0200

    add exercice #1

---
+++ exercices/exercice_1/index.js
@@ -0,0 +1,109 @@
+// Build your own angular !
+
+// Scope
+//  => $$watchers
+//  => $watch
+//  => $digest
+//  => $apply
+
+var Scope = function () {
+
+  this.$$watchers = [];
+  this.$$events = [];
+
+  this.$watch = function(varName, listenerFn) {
+    var watcherFn;
+
+    if (typeof varName === 'function') {
+      watcherFn = varName;
+    } else {
+      watcherFn = function() {
+        return this[varName];
+      };
+    }
+
+    this.$$watchers.push({
+      watcherFn: watcherFn.bind(this),
+      listenerFn: listenerFn
+    });
+  };
+
+  this.$apply = function (func) {
+    // TODO #1
+    // Implémenter la fonction $apply
+    // Le code de cette fonction est très simple et effectue uniquement
+    // un appel à la fonction $digest
+    // Voir les slides pour plus d'infos.
+  };
+
+  this.$digest = function () {
+    // TODO #2
+    // Implémenter la fonction $digest
+    // Le code de cette fonction est un petit peu plus compliqué et nécessite
+    // d'itérer sur tous les watchers et de vérifier si une donnée a changé.
+    // Attention à la première vérification.
+    // Voir les slides pour plus d'infos.
+  };
+};
+
+// Compiler & Directives
+//  => $compile
+//  => $$directives
+
+var $$directives = {
+  'ng-bind': function (scope, element, attributes) {
+  	scope.$watch(attributes['ng-bind'].value, function (newValue) {
+      // TODO #3
+      // Implémenter le code de la fonction.
+      // Cette fonction doit mettre à jour le contenu de l'élément du DOM.
+      // Pour cela, on peut écrire sur l'attribut innerHTML
+      // Par exemple:
+      //   element.innerHTML = 'foo';
+      // -> Ecrit la valeur "foo" dans le DOM
+  	});
+  },
+
+  'ng-model': function (scope, element, attributes) {
+  	scope.$watch(attributes['ng-model'].value, function (newValue) {
+  	  element.value = newValue;
+  	});
+
+  	element.addEventListener('keyup', function () {
+      var name = attributes['ng-model'].value;
+      scope.$apply(function () {
+        // TODO #4
+        // Implémenter le code de la fonction.
+        // Cette fonction doit mettre à jour une variable dans l'objet scope.
+        // Le nom de la variable sur le scope est donné par la variable "name"
+      });
+  	});
+  }
+};
+
+// Ce code permet de déclencher les différentes directives du fichier html.
+// Ce code n'est pas à modifier dans le cadre de l'exercice.
+// Voici son fonctionnement :
+// - Les différentes balises HTML sont analysés afin de vérifier si l'une
+//   d'elle est associée à une directive.
+// - Si c'est le cas, la fonction associée à la directive est déclenchée.
+// - C'est une fonction récursive qui sera appelée sur tous les éléments
+//   de l'arbre du DOM.
+var $compile = function (scope, element) {
+  Array.prototype.forEach.call(element.children, function (children) {
+    $compile(scope, children);
+  });
+
+  Array.prototype.forEach.call(element.attributes, function (attribute) {
+    var attr = attribute.name;
+    var directive = $$directives[attr];
+    if (directive) {
+      directive(scope, element, element.attributes);
+    }
+  });
+};
+
+
+// Tests & logs
+
+var $rootScope = new Scope();
+$compile($rootScope, document.body);
